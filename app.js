3632eab3dae50dfc43864ffa1775443423686de9
MusicJunkyREZ <reidzuckerman@gmail.com>
2019-05-30 14:50:35 -0600

    added security

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express();
 const authRoutes = require("./routes/auth-routes");
-const passportSetup = require("./config/passport-setup")
+const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
+const keys = require("./config/keys")
 
 //set up view engine
 app.set("view engine", "ejs")
+
+// connect to mongodb
+mongoose.connection(keys.mongodb.dbURI)
 
 // set up routes
 app.use("/auth", authRoutes)
